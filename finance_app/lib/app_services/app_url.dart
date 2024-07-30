255b601350fc81aeee6c59cd91961d0fedbcf17d
Sandesh Paudel <sandeshpaudle017@gmai.com>
2024-07-30 21:17:21 +0545

    ..

--- finance_app/lib/app_services/app_url.dart
+++
@@ -1,7 +0,0 @@
-class AppUrl {
-  static const String baseUrl = 'http://127.0.0.1:8000';
-  static const String login = '$baseUrl/login/';
-  static const String register = '$baseUrl/register/';
-  static const String incomes = '$baseUrl/incomes/';
-  static const String expenses = '$baseUrl/expenses/';
-}
